731f787176afd2fcc3c308ab130e0bd12df9d27a
iefcyang <iefcyang@gmail.com>
2022-01-28 17:36:50 +0800

    Add project files.

---
+++ TwoDMorph/MainForm.Designer.cs
@@ -0,0 +1,305 @@
+﻿
+namespace TwoDMorph
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if( disposing && ( components != null ) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent( )
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbNum = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.chtMap = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dgvPoints = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgvDistances = new System.Windows.Forms.DataGridView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnSaveCSV = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chtMap)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPoints)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDistances)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number Of  Key Points";
+            // 
+            // txbNum
+            // 
+            this.txbNum.Location = new System.Drawing.Point(160, 13);
+            this.txbNum.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txbNum.Name = "txbNum";
+            this.txbNum.Size = new System.Drawing.Size(51, 23);
+            this.txbNum.TabIndex = 1;
+            this.txbNum.Text = "7";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer3);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1027, 544);
+            this.splitContainer1.SplitterDistance = 533;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.dgvPoints);
+            this.splitContainer2.Panel1.Controls.Add(this.label2);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.dgvDistances);
+            this.splitContainer2.Panel2.Controls.Add(this.toolStrip1);
+            this.splitContainer2.Panel2.Controls.Add(this.label3);
+            this.splitContainer2.Size = new System.Drawing.Size(490, 544);
+            this.splitContainer2.SplitterDistance = 259;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.btnConfirm);
+            this.splitContainer3.Panel1.Controls.Add(this.label1);
+            this.splitContainer3.Panel1.Controls.Add(this.txbNum);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.chtMap);
+            this.splitContainer3.Size = new System.Drawing.Size(533, 544);
+            this.splitContainer3.SplitterDistance = 117;
+            this.splitContainer3.TabIndex = 2;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(217, 13);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(81, 23);
+            this.btnConfirm.TabIndex = 2;
+            this.btnConfirm.Text = "Confirm";
+            this.toolTip1.SetToolTip(this.btnConfirm, "Click to Reset Map, Create Point and Distance Tables");
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // chtMap
+            // 
+            chartArea1.AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.True;
+            chartArea1.AxisX.Interval = 10D;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.Gainsboro;
+            chartArea1.AxisX.Maximum = 100D;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisX.Title = "x";
+            chartArea1.AxisY.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.True;
+            chartArea1.AxisY.Interval = 10D;
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.Gainsboro;
+            chartArea1.AxisY.Maximum = 100D;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.AxisY.Title = "y";
+            chartArea1.Name = "ChartArea1";
+            this.chtMap.ChartAreas.Add(chartArea1);
+            this.chtMap.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chtMap.Location = new System.Drawing.Point(0, 0);
+            this.chtMap.Name = "chtMap";
+            this.chtMap.Size = new System.Drawing.Size(533, 423);
+            this.chtMap.TabIndex = 0;
+            this.chtMap.Text = "map";
+            this.toolTip1.SetToolTip(this.chtMap, "Successive clicks the generater Sample outlines one by one.");
+            this.chtMap.MouseUp += new System.Windows.Forms.MouseEventHandler(this.chtMap_MouseUp);
+            // 
+            // dgvPoints
+            // 
+            this.dgvPoints.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPoints.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvPoints.Location = new System.Drawing.Point(0, 22);
+            this.dgvPoints.Name = "dgvPoints";
+            this.dgvPoints.RowTemplate.Height = 24;
+            this.dgvPoints.Size = new System.Drawing.Size(490, 237);
+            this.dgvPoints.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(490, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Crossection Point  Data";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(490, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Distances Data";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dgvDistances
+            // 
+            this.dgvDistances.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDistances.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvDistances.Location = new System.Drawing.Point(0, 22);
+            this.dgvDistances.Name = "dgvDistances";
+            this.dgvDistances.RowTemplate.Height = 24;
+            this.dgvDistances.Size = new System.Drawing.Size(490, 234);
+            this.dgvDistances.TabIndex = 3;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnSaveCSV,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 256);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(490, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnSaveCSV
+            // 
+            this.btnSaveCSV.Image = ((System.Drawing.Image)(resources.GetObject("btnSaveCSV.Image")));
+            this.btnSaveCSV.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSaveCSV.Name = "btnSaveCSV";
+            this.btnSaveCSV.Size = new System.Drawing.Size(80, 22);
+            this.btnSaveCSV.Text = "Save CSV";
+            this.btnSaveCSV.ToolTipText = "Save the distance table to a CSV file, which can be opened on Excel to perform PC" +
+    "A and other data analysis.";
+            this.btnSaveCSV.Click += new System.EventHandler(this.btnSaveCSV_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(70, 22);
+            this.toolStripButton2.Text = "Do PCA";
+            this.toolStripButton2.ToolTipText = "Non-implemented Yet";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 544);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Two-D Close-Outline Samples Generator and Point/Distance Table Generators";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel1.PerformLayout();
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chtMap)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPoints)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDistances)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbNum;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chtMap;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.DataGridView dgvPoints;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvDistances;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnSaveCSV;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ TwoDMorph/MainForm.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace TwoDMorph
+{
+    public partial class MainForm : Form
+    {
+        public MainForm( )
+        {
+            InitializeComponent( );
+        }
+        int numberOfPoints = 0;
+        DataTable dttPoints, dttDistances;
+
+        int hitCount = 0;
+        int id = -1;
+        Series s;
+        private void chtMap_MouseUp( object sender, MouseEventArgs e )
+        {
+            PointF p = PointF.Empty;
+            p.X= (float) chtMap.ChartAreas[ 0 ].AxisX.PixelPositionToValue( e.X );
+            p.Y = (float) chtMap.ChartAreas[ 0 ].AxisY.PixelPositionToValue( e.Y );
+            DataPoint dp = new DataPoint( p.X, p.Y );
+            dp.Label = hitCount.ToString( );
+            dp.Tag = p;
+            if( hitCount == 0 )
+            {
+                // create a series, add two points;
+                s = new Series( $"Sample{++id}" );
+                s.ChartType = SeriesChartType.Line;
+                chtMap.Series.Add( s );
+                s.Points.Add( dp );
+                DataPoint dpp = dp.Clone();
+                dpp.Label = "";
+                s.Points.Add( dpp );
+            }
+            else
+            {
+                s.Points.Insert( hitCount, dp );
+            }
+            hitCount++;
+            if( hitCount == numberOfPoints )
+            {
+                // Complete a sample
+                DataRow dr = dttPoints.NewRow( );
+                for( int i = 0 ; i < numberOfPoints ; i++ )
+                    dr[ i ] = s.Points[ i ].Tag;
+                dttPoints.Rows.Add( dr );
+                // 
+                GenerateDistanceRow( id );
+                hitCount = 0;
+            }
+
+        }
+        PointF[ ] pts;
+
+        private void GenerateDistanceRow( int r )
+        {
+            if( pts == null || pts.Length != numberOfPoints )
+                pts = new PointF[ numberOfPoints ];
+            for( int i = 0 ; i < numberOfPoints ; i++ )
+            {
+                pts[i] = (PointF) dttPoints.Rows[ r ][ i ];
+            }
+
+            int c = 0;
+            float x1, y1, x2, y2;
+            DataRow dr =   dttDistances.NewRow( );
+            for( int i = 0 ; i < numberOfPoints ; i++ )
+            {
+                x1 = pts[i].X; y1 = pts[ i ].Y;
+                for( int j = i + 1 ; j < numberOfPoints ;j++ )
+                {
+                    x2 = pts[ j ].X; y2 = pts[ j ].Y;
+                    dr[ c++ ] = Math.Sqrt( ( x2 - x1 ) * ( x2 - x1 ) + ( y2 - y1 ) * ( y2 - y1 ) );
+                }
+            }
+            dttDistances.Rows.Add( dr );
+        }
+
+        private void btnSaveCSV_Click( object sender, EventArgs e )
+        {
+            SaveFileDialog dlg = new SaveFileDialog( );
+            dlg.DefaultExt = "csv";
+            dlg.FileName = "";
+            if( dlg.ShowDialog( ) != DialogResult.OK ) return;
+            WriteTableToCSVFile( dttDistances, dlg.FileName );
+        }
+
+        private void btnConfirm_Click( object sender, EventArgs e )
+        {
+            int num = int.Parse( txbNum.Text );
+            if( numberOfPoints == num ) return;
+            numberOfPoints = num;
+            if( dttPoints == null ) dttPoints = new DataTable( );
+            dttPoints.Rows.Clear( );
+            dttPoints.Columns.Clear( );
+            for( int i = 0 ; i < numberOfPoints ; i++ )
+            {
+                DataColumn dc = new DataColumn( $"P{i}", typeof( PointF ) );
+                dttPoints.Columns.Add( dc );
+            }
+            dgvPoints.DataSource = dttPoints;
+
+            if( dttDistances == null ) dttDistances = new DataTable( );
+            dttDistances.Rows.Clear( );
+            dttDistances.Columns.Clear( );
+
+            for( int i = 0 ; i < numberOfPoints ; i++ )
+            {
+                for( int j = i+1 ; j < numberOfPoints ; j++ )
+                {
+                    DataColumn dc = new DataColumn( $"D{i}-{j}", typeof( double ) );
+                    dttDistances.Columns.Add( dc );
+                }
+            }
+            dgvDistances.DataSource = dttDistances;
+            chtMap.Series.Clear( );
+        }
+
+        void WriteTableToCSVFile( DataTable dtt, string path )
+        {
+             StreamWriter sw = new StreamWriter( path);
+            StringBuilder sb = new StringBuilder( );
+            for( int c = 0 ; c < dtt.Columns.Count ; c++ )
+                if( c == dtt.Columns.Count - 1 )
+                    sb.AppendLine( dtt.Columns[ c ].ColumnName );
+                else
+                    sb.Append( $"{dtt.Columns[ c ].ColumnName}," );
+            for( int r = 0 ; r < dtt.Rows.Count ; r++ )
+            {
+                for( int c = 0 ; c < dtt.Columns.Count ; c++ )
+                    if( c == dtt.Columns.Count - 1 )
+                        sb.AppendLine( dtt.Rows[r][ c ].ToString() );
+                    else
+                        sb.Append( $"{dtt.Rows[ r ][ c ]}," );
+            }
+            sw.Write( sb.ToString( ) );
+            sw.Close( );
+        }
+    }
+}
